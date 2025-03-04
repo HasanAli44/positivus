a05a740ee2c259ca83e950db2ec947fcc23d8f79
HasanAli44 <mdhasanali8439@gmail.com>
2025-03-04 14:54:34 +0600

    design complete

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ var swiper = new Swiper(".Swiperbrand", {
     disableOnInteraction: false,
   },
   breakpoints: {
+    400: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
     600: {
       slidesPerView: 2,
       spaceBetween: 20,
@@ -19,6 +23,31 @@ var swiper = new Swiper(".Swiperbrand", {
     },
     1024: {
       slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
+});
+var swiper = new Swiper(".testimonial", {
+  speed: 600,
+  spaceBetween: 30,
+  loop: true,
+  loop: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
       spaceBetween: 20,
     },
   },
